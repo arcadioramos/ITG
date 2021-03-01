26f0214960d1895f4edf4c1d8a335908343210b8
Carlos-Gaxiola <carlinos1212@gmail.com>
2021-03-01 09:07:50 -0700

    frontend

--- frontend/src/components/NavbarITG.jsx
+++ frontend/src/components/NavbarITG.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+//import Form from 'react-bootstrap/Form'
+//import FormControl from 'react-bootstrap/FormControl'
+//import Button from 'react-bootstrap/Button'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../src/App.css'
@@ -13,12 +13,12 @@ class NavbarITG extends Component {
   render() {
     return (
       <>
-        <Navbar className="color-nav" collapseOnSelect expand="lg" bg="primary" variant="dark">
-          <Navbar.Brand href="#home">INSTITUTO TECNOLÓGICO DE GUAYMAS</Navbar.Brand>
+        <Navbar className="color-nav justify-content-center" collapseOnSelect expand="lg" bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Inicio</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
+          <Navbar.Collapse id="responsive-navbar-nav justify-content-center">
             <Nav className="mr-auto">
-              <Nav.Link className="color-nav" href="#features">Institución</Nav.Link>
+              <Nav.Link className="color-nav " href="#features">Institución</Nav.Link>
               <NavDropdown className="color-nav" title="Oferta académica" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Licenciado en administración</NavDropdown.Item>
                 
@@ -27,12 +27,10 @@ class NavbarITG extends Component {
                 <NavDropdown.Item href="#action/3.1">Hackathon</NavDropdown.Item>
                 
               </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link className="color-nav" href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
+              <NavDropdown className="color-nav" title="Servicios" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Egresados</NavDropdown.Item>
+                
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
